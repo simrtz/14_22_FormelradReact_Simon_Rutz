906671b8b6b665c1705372b229f300aefac71e51
simrtz <simon.rutz@outlook.de>
2023-06-09 22:28:20 +0200

    A message field is required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -8,7 +8,8 @@ export default function Formelrad() {
         u: 10,
         i: 2,
         r: "",
-        p: ""
+        p: "",
+        message: ""
     })
 
     const handleSubmit = (event) => {
@@ -54,6 +55,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <p>{values.message}</p>
                 </form>
             </section>
         </>
